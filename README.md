6c763697c89fd0e633bcf62d8bff3bcbe84043e3
Lapis_Mine_Voyager <simbolerisgod@yahoo.co.jp>
2018-10-09 14:46:30 +0900

    i3 config add

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # dotfiles
 ≡(∈ 'o')
 vimrc, bashrc, latexmkrc,...
+
+win
+$ mklink linkname target_path
+
--- paste_link.sh
+++ paste_link.sh
@@ -14,3 +14,8 @@ ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.latexmkrc ~/.latexmkrc
 ln -s ~/dotfiles/.profile ~/.profile
 
+mkdir ~/.config/i3
+ln -s ~/dotfiles/i3_files/i3/config ~/.config/i3/config
+mkdir ~/.config/i3status
+ln -s ~/dotfiles/i3_files/i3status/config ~/.config/i3status/config
+
