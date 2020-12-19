59f99a782107a6acaffc7c2af834c0023e7aa321
Selenite <hoge>
2020-12-19 15:34:05 +0900

    wget for git

--- paste_link.sh
+++ paste_link.sh
@@ -6,6 +6,8 @@ rm ~/.vimrc
 rm ~/.bashrc
 rm ~/.latexmkrc
 rm ~/.profile
+wget https://raw.github.com/git/git/master/contrib/completion/git-completion.bash
+wget https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh
 
 git clone https://github.com/Shougo/neobundle.vim ~/dotfiles/.vim/bundle/neobundle.vim
 ln -s ~/dotfiles/.vim ~/.vim
